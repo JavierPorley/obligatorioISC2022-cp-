87a8d7f9783cead313f2033879a101f569445f47
JavierPorley <69149517+JavierPorley@users.noreply.github.com>
2022-06-27 22:05:15 -0300

    Update instances.tf

--- OB/instances.tf
+++ OB/instances.tf
@@ -4,7 +4,7 @@ resource "aws_instance" "bastion" {
   ami                         = var.ami
   instance_type               = var.instance_type
   key_name                    = var.key_name
-  vpc_security_group_ids      = [aws_security_group.sg2.id, aws_security_group.sg3.id]
+  vpc_security_group_ids      = [aws_security_group.sg_bastion.id]
   subnet_id                   = aws_subnet.sub-1c.id
   associate_public_ip_address = true
 
